943f7709f0227f790a4040e7b7dde7d29047983d
lubomskirafal <lubomski.rafal@gmail.com>
2020-09-29 11:42:04 +0200

    add nav and header html, css, js

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,24 @@ document.addEventListener('DOMContentLoaded', ()=> {
 const hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', ()=> {
-    handleHamburgerClick(hamburger);
+    handleHamburgerClick();
 })
-const manu = document.querySelector('nav');
+const manu = document.querySelector('.manu');
+const manuBgListener = document.querySelector('.manu li:nth-child(1)')
 
 
-const handleHamburgerClick = (hamburger)=> {
-    // manu.classList.toggle('manu__list--active');
-    // manu.addEventListener('click', ()=>handleHamburgerClick(hamburger));
+const handleHamburgerClick = ()=> {
+
+    if(!manu.classList.contains('manu--active')) {
+        manu.classList.remove('manu--noActive')
+        manu.classList.add('manu--active')
+    }else {
+        manu.classList.add('manu--noActive')
+        manu.classList.remove('manu--active');
+    };
+
+    manuBgListener.addEventListener('click', ()=>handleHamburgerClick());
+
     if(!hamburger.classList.contains('hamburger--active')) {
         hamburger.classList.remove('hamburger--noActive')
         hamburger.classList.add('hamburger--active')
@@ -24,4 +34,13 @@ const handleHamburgerClick = (hamburger)=> {
         hamburger.classList.remove('hamburger--active');
     };
 }; 
+
+window.addEventListener('resize', ()=> {
+    if(window.innerWidth > 1200) {
+        manu.classList.remove('manu--active');
+        manu.classList.remove('manu--noActive');
+        hamburger.classList.remove('hamburger--active');
+        hamburger.classList.remove('hamburger--noActive');
+    }
+})
 
